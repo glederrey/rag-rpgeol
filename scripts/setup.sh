7a922ac571b1b357087dd72f1fcf068580ca8dc0
glederrey <glederre@gmail.com>
2025-08-27 17:42:27 +0200

    [UPD] setup reminder

---
+++ scripts/setup.sh
@@ -0,0 +1,10 @@
+azd auth login
+azd env set USE_CHAT_HISTORY_BROWSER true
+azd env set ENABLE_LANGUAGE_PICKER true
+azd env set AZURE_SEARCH_SEMANTIC_RANKER standard
+azd env set AZURE_SEARCH_QUERY_REWRITING true
+
+# Blocked by some permissions to have authentication => TO BE FIXED
+#azd env set AZURE_AUTH_TENANT_ID <TENANT_ID>
+#azd env set AZURE_USE_AUTHENTICATION true
+#azd env set USE_CHAT_HISTORY_COSMOS true
